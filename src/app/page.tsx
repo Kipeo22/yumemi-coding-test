c58bc45109ac3ca496521f78d43749389b9bdbb2
Kipeo <kp225048@gmail.com>
2025-03-12 13:35:14 +0900

    都道府県一覧を取得しチェックボックス表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import React from "react";
+import PrefectureSelector from "@/app/components/PrefectureSelector";
+
 export default function Home() {
   return (
     <div>
       <h1>ゆめみフロントエンド コーディングテスト</h1>
+      <PrefectureSelector />
     </div>
   );
 }
