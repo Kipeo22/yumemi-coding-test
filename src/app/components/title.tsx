525feed9d640037e71161aa74fca59ea7277672f
Kipeo <kp225048@gmail.com>
2025-03-13 01:08:29 +0900

    css記述

---
+++ src/app/components/title.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const title = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        backgroundColor: "lightblue",
+        padding: "10px",
+      }}
+    >
+      <h1>都道府県別の人口推移</h1>
+    </div>
+  );
+};
+
+export default title;
